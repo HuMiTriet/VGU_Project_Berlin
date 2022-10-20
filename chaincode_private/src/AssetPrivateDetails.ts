75c79ddd25f2ee8851925bfba1e91bf1a707bfcd
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2022-10-20 23:00:34 +0700

    init code

---
+++ chaincode_private/src/AssetPrivateDetails.ts
@@ -0,0 +1,42 @@
+
+// @DataType()
+// class AssetPrivateDetails {
+
+//     @Property()
+//     private final String assetID;
+
+//     @Property()
+//     private int appraisedValue;
+
+//     public String getAssetID() {
+//         return assetID;
+//     }
+
+//     public int getAppraisedValue() {
+//         return appraisedValue;
+//     }
+
+//     public AssetPrivateDetails(final String assetID,
+//         final int appraisedValue) {
+//         this.assetID = assetID;
+//         this.appraisedValue = appraisedValue;
+//     }
+
+//     public byte[] serialize() {
+//         String jsonStr = new JSONObject(this).toString();
+//         return jsonStr.getBytes(UTF_8);
+//     }
+
+//     public static AssetPrivateDetails deserialize(final byte[] assetJSON) {
+//         try {
+//             JSONObject json = new JSONObject(new String(assetJSON, UTF_8));
+//             final String id = json.getString("assetID");
+//             final int appraisedValue = json.getInt("appraisedValue");
+//             return new AssetPrivateDetails(id, appraisedValue);
+//         } catch (Exception e) {
+//             throw new ChaincodeException("Deserialize error: " + e.getMessage(), "DATA_ERROR");
+//         }
+//     }
+
+
+// }
