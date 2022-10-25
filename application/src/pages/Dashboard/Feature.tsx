0ea92a2c7b0070c28a14204bba301819299129b6
Nguyễn Khoa <nguyenkhoa090401@gmail.com>
2022-10-25 22:27:20 +0700

    dynamic dashboard

--- application/src/pages/Dashboard/Feature.tsx
+++ application/src/pages/Dashboard/Feature.tsx
@@ -2,63 +2,108 @@ import './Dashboard.css'
 import Homepage_House from "../../assets/Homepage_House1.jpg";
 
 import { Row, Col, Card, Pagination } from 'antd';
+import { RealEstate } from '../../resources/realEstate';
 
 function AppFeature() {
-  return (
+  var html = [];
+  const realEstates:Array<RealEstate> = [
+  {
+    "area": 300,
+    "id": "asset1",
+    "location": "Ben Cat 941",
+    "membershipThreshold": 0,
+    "owners": [
+      {
+        "isSeller": true,
+          "ownerID": "user2",
+          "ownershipPercentage": 100,
+          "sellPercentage": 50,
+          "sellPrice": 2000,
+          "sellThreshold": 5
+      },
+        {
+            "isSeller": true,
+            "ownerID": "user1",
+            "ownershipPercentage": 100,
+            "sellPercentage": 50,
+            "sellPrice": 1000,
+            "sellThreshold": 5
+        },
+        {
+          "isSeller": true,
+          "ownerID": "user1",
+          "ownershipPercentage": 100,
+          "sellPercentage": 10,
+          "sellPrice": 100000,
+          "sellThreshold": 5
+      }
+        
+    ],
+    "roomList": {
+        "numOfBathroom": 2,
+        "numOfBedroom": 2,
+        "numOfDiningroom": 1,
+        "numOfLivingroom": 1
+    }
+},
+{
+  "area": 400,
+  "id": "asset2",
+  "location": "Ben Cat 941",
+  "membershipThreshold": 0,
+  "owners": [
+    {
+      "isSeller": true,
+        "ownerID": "user2",
+        "ownershipPercentage": 100,
+        "sellPercentage": 50,
+        "sellPrice": 2000,
+        "sellThreshold": 5
+    },
+      {
+          "isSeller": true,
+          "ownerID": "user1",
+          "ownershipPercentage": 100,
+          "sellPercentage": 50,
+          "sellPrice": 1000,
+          "sellThreshold": 5
+      },
+      {
+        "isSeller": true,
+        "ownerID": "user1",
+        "ownershipPercentage": 100,
+        "sellPercentage": 10,
+        "sellPrice": 100000,
+        "sellThreshold": 5
+    }
+      
+  ],
+  "roomList": {
+      "numOfBathroom": 2,
+      "numOfBedroom": 2,
+      "numOfDiningroom": 1,
+      "numOfLivingroom": 1
+  }
+}]
+realEstates.forEach(function (testRealEstate:any) {
+  console.log(testRealEstate.id);
+ 
+  let prices:Array<number> =[]
+  testRealEstate.owners.forEach(function (value:any) {
+    prices.push(value.sellPrice * 100 / value.sellPercentage)
+  }); 
+  prices = prices.sort((n1,n2) => n1 - n2)
+
+  html.push ((
     <div id="feature">
       <div className="container-fluid">
         <Row gutter={[16, 16]}>
           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
             <Card style={{borderRadius: "20px", overflow: "hidden"}} hoverable cover={<img alt="City Garden Duplex " src={Homepage_House} />}>
-            <div className="houseType">City Garden Duplex</div>
-              <p>Ngo Tat To str., Binh Thanh Dist.
-                <br/>100m2
-                <div className="price">6.000.000.000 vnd</div>
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-          <Card style={{borderRadius: "20px", overflow: "hidden"}} hoverable cover={<img alt="City Garden Duplex " src={Homepage_House} />}>
-              <div className="houseType">City Garden Duplex</div>
-              <p>Ngo Tat To str., Binh Thanh Dist.
-                <br/>100m2
-                <div className="price">6.000.000.000 vnd</div>
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-          <Card style={{borderRadius: "20px", overflow: "hidden"}} hoverable cover={<img alt="City Garden Duplex " src={Homepage_House} />}>
-              <div className="houseType">City Garden Duplex</div>
-              <p>Ngo Tat To str., Binh Thanh Dist.
-                <br/>100m2
-                <div className="price">6.000.000.000 vnd</div>
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-          <Card style={{borderRadius: "20px", overflow: "hidden"}} hoverable cover={<img alt="City Garden Duplex " src={Homepage_House} />}>
-              <div className="houseType">City Garden Duplex</div>
-              <p>Ngo Tat To str., Binh Thanh Dist.
-                <br/>100m2
-                <div className="price">6.000.000.000 vnd</div>
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-          <Card style={{borderRadius: "20px", overflow: "hidden"}} hoverable cover={<img alt="City Garden Duplex " src={Homepage_House} />}>
-              <div className="houseType">City Garden Duplex</div>
-              <p>Ngo Tat To str., Binh Thanh Dist.
-                <br/>100m2
-                <div className="price">6.000.000.000 vnd</div>
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-          <Card style={{borderRadius: "20px", overflow: "hidden"}} hoverable cover={<img alt="City Garden Duplex " src={Homepage_House} />}>
-              <div className="houseType">City Garden Duplex</div>
-              <p>Ngo Tat To str., Binh Thanh Dist.
-                <br/>100m2
-                <div className="price">6.000.000.000 vnd</div>
+            <div className="houseType">{testRealEstate.id}</div>
+              <p>{testRealEstate.location}
+                <br/>{testRealEstate.area} m<sup>2</sup>
+                <div className="price">{prices[0]} - {prices[prices.length-1]} vnd</div>
               </p>
             </Card>
           </Col>
@@ -66,7 +111,9 @@ function AppFeature() {
         <div className="paginationBlock"><Pagination defaultCurrent={1} total={50} /></div>
       </div>
     </div>
-  );
+  ))
+});
+  return <div> {html} </div>;
 }
 
 export default AppFeature;
--- application/src/resources/realEstate.ts
+++ application/src/resources/realEstate.ts
@@ -3,7 +3,6 @@ import { RoomType } from './roomType'
 
 export interface RealEstate {
   id: string
-  doctype?: string
   roomList: RoomType
   area: number
   location: string
