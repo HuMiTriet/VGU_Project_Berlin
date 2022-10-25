61fa021b44958a32291dd40b20f33a2d06c9d160
danduong17 <16079@student.vgu.edu.vn>
2022-10-25 08:18:32 +0700

    Create NavElements - Nav & NavLink

---
+++ application/src/components/Navbar/NavbarElements.ts
@@ -0,0 +1,25 @@
+import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background: #ffffff;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10;
+`;
+
+export const NavLink = styled(Link)`
+  color: #370257;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    color: #370257;
+  }
+`;
