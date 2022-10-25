1760e2da51eff4afd2186eec78e7ca92be0afff7
danduong17 <16079@student.vgu.edu.vn>
2022-10-25 08:26:18 +0700

    Add NavBtn & NavBtnLink

--- application/src/components/Navbar/NavbarElements.ts
+++ application/src/components/Navbar/NavbarElements.ts
@@ -49,3 +49,32 @@ export const NavMenu = styled.div`
     display: none;
   }
 `;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 60px;
+  background: #d5c1ff;
+  font-weight: bold;
+  padding: 10px 22px;
+  color: #000;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #370257;
+    color: #ffffff;
+  }
+`;
