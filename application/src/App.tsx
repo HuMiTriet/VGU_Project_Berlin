9c9f9aa27a876ad1d5a3e5605e16108a0410a848
Thai Hoang Tam <hoangtam3062002@gmail.com>
2022-10-25 20:32:12 +0700

    Change old dashboard to test dashboard

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,30 +1,22 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-//import Home from './pages/Home'
-import Contact from './pages/Contract/Contract'
+import Home from './pages/Home'
+import Contract from './pages/Contract/Contract'
 import Dashboard from './pages/Dashboard/Dashboard'
-// import Login from './Authentication/Login/Login'
-// import Dashboard from './Authentication/Dashboard/Dashboard'
-// import Reset from './Authentication/Reset/Reset'
-// import Register from './Authentication/Register/Register'
+import TestDashboard from './DashboardTest/Dashboard'
+import Login from './Authentication/Login/Login'
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-          {/*<Route path="/" element={<Home />} />*/}
-          <Route path="/" element={<Contact />} />
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/reset" element={<Reset />} />
-          <Route path="/dashboard" element={<Dashboard />} /> */}
-
-          {/* Order of pages
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboardtest" element={<TestDashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/propertyinfo" element={<PropertyInfo />} />
-          <Route path="/Contract" element={<Contract />} /> */}
+          {/* <Route path="/propertyinfo" element={<PropertyInfo />} /> */}
+          <Route path="/contract" element={<Contract />} />
         </Routes>
       </Router>
     </div>
