e20ee0dc8f58320b80429c4a33cda095caa8a07e
danduong17 <16079@student.vgu.edu.vn>
2022-10-25 08:34:17 +0700

    Add Home into App.tsx

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './Authentication/Login/Login'
-import Dashboard from './Authentication/Dashboard/Dashboard'
-import Reset from './Authentication/Reset/Reset'
-import Register from './Authentication/Register/Register'
+import Home from './pages/Home'
+// import Login from './Authentication/Login/Login'
+// import Dashboard from './Authentication/Dashboard/Dashboard'
+// import Reset from './Authentication/Reset/Reset'
+// import Register from './Authentication/Register/Register'
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/reset" element={<Reset />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
         </Routes>
       </Router>
     </div>
