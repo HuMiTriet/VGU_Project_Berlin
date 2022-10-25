38770ba37ff9c951780141cb84e88688a23b4df2
danduong17 <16079@student.vgu.edu.vn>
2022-10-25 08:20:15 +0700

    Add NavMenu & Bars

--- application/src/components/Navbar/NavbarElements.ts
+++ application/src/components/Navbar/NavbarElements.ts
@@ -1,3 +1,4 @@
+import { FaBars } from "react-icons/fa";
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -21,5 +22,30 @@ export const NavLink = styled(Link)`
 
   &.active {
     color: #370257;
+  }
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #000;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
   }
 `;
