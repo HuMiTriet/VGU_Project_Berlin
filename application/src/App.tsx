8be66b4f0c7ffb6e3f197253c0af46060b75fb50
vunhatquang1001 <vunhatquang1001@gmail.com>
2022-10-27 21:39:42 +0700

    update contract page and upload page

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,47 +1,38 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
 //import Home from './pages/Home'
-import Contact from './pages/Contract/Contract'
 //import Upload from './pages/InfoUpload/InfoUpload'
 //import Dashboard from './pages/Dashboard/Dashboard'
 // import Login from './Authentication/Login/Login'
 // import Dashboard from './Authentication/Dashboard/Dashboard'
 // import Reset from './Authentication/Reset/Reset'
 // import Register from './Authentication/Register/Register'
-=======
-import Home from './pages/Home'
-import Contract from './pages/Contract/Contract'
-import Dashboard from './pages/Dashboard/Dashboard'
-import TestDashboard from './DashboardTest/Dashboard'
-import Login from './Authentication/Login/Login'
->>>>>>> 7ba922381282edcd1ab403119168a00ef6fedc8a
+//import TestDashboard from './DashboardTest/Dashboard'
+//import Contract from './pages/Contract/Contract'
+import InfoUpload from './pages/InfoUpload/InfoUpload'
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-<<<<<<< HEAD
           {/*<Route path="/" element={<Home />} />
           <Route path="/" element={<Upload />} />
           */}
-          <Route path="/" element={<Contact />} />
+          <Route path="/" element={<InfoUpload />} />
           {/* <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Contract />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/reset" element={<Reset />} />
          */}
           
           {/* Order of pages
-=======
           <Route path="/" element={<Home />} />
->>>>>>> 7ba922381282edcd1ab403119168a00ef6fedc8a
           <Route path="/login" element={<Login />} />
           <Route path="/dashboardtest" element={<TestDashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
           {/* <Route path="/propertyinfo" element={<PropertyInfo />} /> */}
-          <Route path="/contract" element={<Contract />} />
         </Routes>
       </Router>
     </div>
--- application/src/pages/InfoUpload/InfoUpload.tsx
+++ application/src/pages/InfoUpload/InfoUpload.tsx
@@ -1,90 +1,171 @@
 import Navbar from "../../components/Navbar";
 import './InfoUpload.css'
-import { Form, Input, Button, Select, Col, Row, Card} from 'antd';
+import { Form, Input, Button, Select, Col, Row, Card, Upload, Typography} from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+const { TextArea } = Input;
 
-const InfoUpload = () => {
+
+
+const normFile = (e: any) => {
+  console.log('Upload event:', e);
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+
+
+const InfoUpload = (props) => {
+
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <> <Navbar/> 
-    <Row align='middle' justify='center'>
-    <div id="upload" className="block contactBlock">
-      <Card bodyStyle={{ backgroundColor: '#F3F3FA'}} style={{borderRadius: "20px", overflow: "hidden", width: 900}}>
-        <div className="container-fluid">
-          <div className="titleHolder">
-            <h2>Upload</h2>
+  <> <Navbar/> 
+    <Row justify='center'>
+      <div id="upload" className="block contactBlock">
+        <Card bodyStyle={{ backgroundColor: '#F3F3FA'}} style={{borderRadius: "20px", overflow: "hidden", width: 900}}>
+          <div className="container-fluid">
+
+            <div className="titleHolder">
+              <Typography>
+                <h1>Upload</h1>
+              </Typography>
+            </div>
+
+            <Row >
+              <Col span={12}>
+                <Form onFinish={(values) => {
+                  console.log({ values });
+                  }}
+                  onFinishFailed={(error) => {
+                  console.log({ error });
+                  }} initialValues={{ remember: true }}>
+
+                  <h3>Number of Rooms</h3>
+                  <Form.Item
+                    name="number of rooms"
+                    rules={[
+                      {
+                        type: 'string',
+                        message: 'The input is not valid',
+                      },
+                      { 
+                        required: true,
+                        message: 'Please enter Number of Rooms!' 
+                      }]
+                    }
+                  >
+                    <Input placeholder="Number of Rooms" />
+                  </Form.Item>
+
+                  <h3>Floors</h3>
+                  <Form.Item
+                    name="floors"
+                    rules={[
+                      {
+                        type: 'string',
+                        message: 'The input is not valid',
+                      },
+                      {
+                        required: true,
+                        message: 'Please input Total number!',
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Total"/>
+                  </Form.Item>
+
+                  <h3>Description</h3>
+                  <Form.Item>
+                    <TextArea rows={4} />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary">
+                      Confirm
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Col>
+
+              <Col span={12}>
+                <Form onFinish={(values) => {
+                  console.log({ values });
+                  }}
+                  onFinishFailed={(error) => {
+                  console.log({ error });
+                  }} initialValues={{ remember: true }}>
+                    <h3>Area</h3>
+                    <Form.Item
+                      name="area"
+                      rules={[
+                        {
+                          type: 'string',
+                          message: 'The input is not valid',
+                        },
+                        {
+                          required: true,
+                          message: 'Please input Area!',
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Area"/>
+                    </Form.Item>
+                    
+                    <h3>Terrace Area</h3>
+                    <Form.Item
+                      name="terrace area"
+                      rules={[
+                        {
+                          type: 'string',
+                          message: 'The input is not valid',
+                        },
+                        {
+                          required: true,
+                          message: 'Please input Terrace Area!',
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Terrace Area"/>
+                    </Form.Item>
+
+                    <h3>Balcony Area</h3>
+                    <Form.Item
+                      name="balcony area"
+                      rules={[
+                        {
+                          type: 'string',
+                          message: 'The input is not valid',
+                        },
+                        {
+                          required: true,
+                          message: 'Please input Balcony Area!',
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Balcony Area"/>
+                    </Form.Item>
+
+                  <h3>Upload File</h3>         
+                  <Form.Item name="upload file" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                    <Upload.Dragger name="files" action="/upload.do">
+                      <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                      </p>
+                      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                      <p className="ant-upload-hint">Support for a single or bulk upload.</p> 
+                    </Upload.Dragger>
+                  </Form.Item>
+
+                </Form>
+              </Col>
+            </Row>
           </div>
-          <Row >
-            <Col span={12}>
-            
-            </Col>
-            <Col span={12}>
-            <Form initialValues={{ remember: true }}>
-              <div>
-                <h3>Buy Percentage</h3>
-                <Form.Item
-                  name="Buy Percentage"
-                  rules={[
-                    {
-                      type: 'string',
-                      message: 'The input is not valid',
-                    },
-                    { 
-                      required: true,
-                      message: 'Please enter Buy Percentage!' 
-                    }]
-                  }
-                >
-                  <Input placeholder="Buy Percentage" />
-                </Form.Item>
-              </div>
-              <div>
-                <h3>Total</h3>
-                <Form.Item
-                  name="total"
-                  rules={[
-                    {
-                      type: 'string',
-                      message: 'The input is not valid',
-                    },
-                    {
-                      required: true,
-                      message: 'Please input Total number!',
-                    },
-                  ]}
-                >
-                  <Input placeholder="Total"/>
-                </Form.Item>
-              </div>
-              <div>
-              <h3>Other Services</h3>
-              <Form.Item>
-                <Select placeholder="Cleaning">
-                    <Select.Option value="Cleaning">Cleaning</Select.Option>
-                    <Select.Option value="Insurance">Insurance</Select.Option>
-                </Select>
-              </Form.Item>
-            </div>
-            <div>
-              <h3>Payment</h3>
-              <Form.Item>
-                <Select placeholder="One-Time-Payment">
-                    <Select.Option value="One-Time-Payment">One-Time-Payment</Select.Option>
-                    <Select.Option value="Installment">Installment</Select.Option>
-                </Select>
-              </Form.Item>
-            </div>
-            <Form.Item>
-              <Button type="primary">
-                Confirm
-              </Button>
-            </Form.Item>
-          </Form>
-            </Col>
-          </Row>
-        </div>
-      </Card>
-    </div>
+        </Card>
+      </div>
     </Row>
-    </>
+  </>
   );
 }
 
