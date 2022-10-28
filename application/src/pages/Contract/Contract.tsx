c128f7ac81cf033d73a305ae93d74b22ea65c772
Thai Hoang Tam <hoangtam3062002@gmail.com>
2022-10-28 11:51:48 +0700

    Load userID from dashboard, can transfer realEstate

--- application/src/pages/Contract/Contract.tsx
+++ application/src/pages/Contract/Contract.tsx
@@ -23,61 +23,35 @@ import './Contract.css'
  */
 function Contract() {
   const [showAlert, setShowAlert] = useState(false)
-  const [user, loading] = useAuthState(auth)
-  const [idLoading, setLoading] = useState(true)
-  const [buyerID, setID] = useState('')
+  const [result, loadResult] = useState('')
+  // const [idLoading, setLoading] = useState(true)
+  const buyerID = localStorage['userID']
   const realEstateID = localStorage['realEstateID']
   const sellerID = localStorage['sellerID']
   const ownershipPercentage = localStorage['ownershipPercentage']
   const sellPercentage = localStorage['sellPercentage']
   const sellThreshold = localStorage['sellThreshold']
   const sellPrice = localStorage['sellPrice']
-  const fetchId = async () => {
-    try {
-      const q = query(collection(db, 'users'), where('uid', '==', user?.uid))
-      const doc = await getDocs(q)
-      const data = doc.docs[0].data()
-      setID(data.realEstateID)
-      setLoading(false)
-      console.log(realEstateID)
-    } catch (err) {
-      console.error(err)
-      // alert('An error occured while fetching user data')
-    }
+  const transferRealEstateResult = function (buyPercentage, value) {
+    api
+      .transferRealEstate(realEstateID, sellerID, buyerID, buyPercentage, value)
+      .then(allData => {
+        loadResult(allData)
+        return
+      })
+      .catch((error: unknown) => {
+        console.log(error)
+      })
   }
-  useEffect(() => {
-    fetchId()
-  })
   const onFinish = (e: unknown) => {
-    // let realEstateID:string
-    // let buyerID: string
     const buyPercentage: string = e['Buy Percentage']
     const value = (
       (parseInt(sellPrice) / 100) *
       parseInt(buyPercentage)
     ).toString()
-    const [result, loadResult] = useState('')
-    const transferRealEstateResult = function () {
-      api
-        .transferRealEstate(
-          realEstateID,
-          sellerID,
-          buyerID,
-          buyPercentage,
-          value
-        )
-        .then(allData => {
-          loadResult(allData)
-          return
-        })
-        .catch((error: unknown) => {
-          console.log(error)
-        })
-    }
     try {
-      transferRealEstateResult()
+      transferRealEstateResult(buyPercentage, value)
     } catch (err) {
-      // err box
       console.log(err)
     }
     console.log(e)
@@ -156,7 +130,12 @@ function Contract() {
                   </Card>
                 </Col>
                 <Col span={12}>
-                  <Form onFinish={onFinish} initialValues={{ remember: true }}>
+                  <Form
+                    onFinish={e => {
+                      onFinish(e)
+                    }}
+                    initialValues={{ remember: true }}
+                  >
                     <div>
                       <h3>Buy Percentage</h3>
                       <Form.Item
@@ -210,7 +189,6 @@ function Contract() {
                     <div>
                       <h3>Payment Method</h3>
                       <Form.Item
-
                         name="Payment Method"
                         rules={[
                           {
@@ -263,11 +241,7 @@ function Contract() {
     </>
   )
 
-  if (idLoading) {
-    return <div>Loading</div>
-  } else {
-    return html
-  }
+  return html
 }
 
 export default Contract
