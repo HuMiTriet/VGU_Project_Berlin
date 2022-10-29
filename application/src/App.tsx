784b133bf312c624b4c6bbf719d9f366ef2c9283
danduong17 <16079@student.vgu.edu.vn>
2022-10-29 09:23:57 +0700

    Fix bugs in PropertyDetail

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import Home from './pages/Home'
 // import Contract from './pages/Contract/Contract'
