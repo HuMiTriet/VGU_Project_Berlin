b226e0fff057e1cd3fca7f7086182a8ee334782e
vunhatquang1001 <vunhatquang1001@gmail.com>
2022-10-31 10:19:01 +0700

    update InfoUpload page

--- application/src/pages/InfoUpload/InfoUpload.tsx
+++ application/src/pages/InfoUpload/InfoUpload.tsx
@@ -1,7 +1,6 @@
 import Navbar from "../../components/Navbar";
 import './InfoUpload.css'
 import { Form, Input, Button, Select, Col, Row, Card, Upload, Typography} from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
 const { TextArea } = Input;
 
 
@@ -35,165 +34,148 @@ const InfoUpload = () => {
               </Typography>
             </div>
 
-            <Row >
-              <Col span={12}>
-                <Form onFinish={(value) => {
-                  console.log({ value });
-                  }}
-                  onFinishFailed={(error) => {
-                  console.log({ error });
-                  }} >
-
-                  <h3>Tier List</h3>
-                  <Form.Item name = "tier" rules={[{ required: true, message: 'Tier list is required' }]}>
-                    <Select placeholder="Tier List">  
-                        <Select.Option value="tier1">Tier 1</Select.Option>
-                        <Select.Option value="tier2">Tier 2</Select.Option>
-                        <Select.Option value="tier3">Tier 3</Select.Option>
-                    </Select>
-                  </Form.Item>
-
-                  <h3>Price</h3>
-                    <Form.Item
-                      name="price"
-                      rules={[
-                        {
-                          type: 'string',
-                          message: 'The input is not valid',
-                        },
-                        { 
-                          required: true,
-                          message: 'Please enter the Price!' 
-                        }]
-                      }
-                    >
-                      <Input placeholder="Price" />
-                    </Form.Item>
-
-                  <h3>Number of Rooms</h3>
-                  <Form.Item
-                    name="rooms"
-                    rules={[
-                      {
-                        type: 'string',
-                        message: 'The input is not valid',
-                      },
-                      { 
-                        required: true,
-                        message: 'Please enter Number of Rooms!' 
-                      }]
-                    }
-                  >
-                    <Input placeholder="Number of Rooms" />
-                  </Form.Item>
-
-                  <h3>Floors</h3>
-                  <Form.Item
-                    name="floors"
-                    rules={[
-                      {
-                        type: 'string',
-                        message: 'The input is not valid',
-                      },
-                      {
-                        required: true,
-                        message: 'Please input Total number!',
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Total"/>
-                  </Form.Item>
-
-                  <h3>Description</h3>
-                  <Form.Item name= "description">
-                    <TextArea rows={4} />
-                  </Form.Item>
-                  <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                      Confirm
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </Col>
-
-              <Col span={12}>
-                <Form onFinish={(value) => {
-                  console.log({ value });
-                  }}
-                  onFinishFailed={(error) => {
-                  console.log({ error });
-                  }}>
-
-                    <h3>Area</h3>
-                    <Form.Item
-                      name="area"
-                      rules={[
-                        {
-                          type: 'string',
-                          message: 'The input is not valid',
-                        },
-                        {
-                          required: true,
-                          message: 'Please input Area!',
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Area"/>
-                    </Form.Item>
+            <Form onFinish={(value) => {
+              console.log({ value });
+              }}
+              onFinishFailed={(error) => {
+              console.log({ error });
+              }} >
                     
-                    <h3>Terrace Area</h3>
-                    <Form.Item
-                      name="terraceArea"
-                      rules={[
-                        {
-                          type: 'string',
-                          message: 'The input is not valid',
-                        },
-                        {
-                          required: true,
-                          message: 'Please input Terrace Area!',
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Terrace Area"/>
-                    </Form.Item>
+                <Row >
+                  <Col span={12}>
 
-                    <h3>Balcony Area</h3>
-                    <Form.Item
-                      name="balconyArea"
-                      rules={[
-                        {
-                          type: 'string',
-                          message: 'The input is not valid',
-                        },
-                        {
-                          required: true,
-                          message: 'Please input Balcony Area!',
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Balcony Area"/>
-                    </Form.Item>
+                      <h3>Tier List</h3>
+                      <Form.Item name = "tier" rules={[{ required: true, message: 'Tier list is required' }]}>
+                        <Select placeholder="Tier List">  
+                            <Select.Option value="tier1">Tier 1</Select.Option>
+                            <Select.Option value="tier2">Tier 2</Select.Option>
+                            <Select.Option value="tier3">Tier 3</Select.Option>
+                        </Select>
+                      </Form.Item>
 
-                  <h3>Upload File</h3>         
-                  <Form.Item name="uploadFile" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
-                    <Upload.Dragger name="files" action="/upload.do">
-                      <p className="ant-upload-drag-icon">
-                        <InboxOutlined />
-                      </p>
-                      <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                      <p className="ant-upload-hint">Support for a single or bulk upload.</p> 
-                    </Upload.Dragger>
-                  </Form.Item>
-                  
+                      <h3>Price</h3>
+                        <Form.Item
+                          name="price"
+                          rules={[
+                            {
+                              type: 'string',
+                              message: 'The input is not valid',
+                            },
+                            { 
+                              required: true,
+                              message: 'Please enter the Price!' 
+                            }]
+                          }
+                        >
+                          <Input placeholder="Price" />
+                        </Form.Item>
+
+                      <h3>Number of Rooms</h3>
+                      <Form.Item
+                        name="rooms"
+                        rules={[
+                          {
+                            type: 'string',
+                            message: 'The input is not valid',
+                          },
+                          { 
+                            required: true,
+                            message: 'Please enter Number of Rooms!' 
+                          }]
+                        }
+                      >
+                        <Input placeholder="Number of Rooms" />
+                      </Form.Item>
+
+                      <h3>Floors</h3>
+                      <Form.Item
+                        name="floors"
+                        rules={[
+                          {
+                            type: 'string',
+                            message: 'The input is not valid',
+                          },
+                          {
+                            required: true,
+                            message: 'Please input Total number!',
+                          },
+                        ]}
+                      >
+                        <Input placeholder="Total"/>
+                      </Form.Item>
+                    
+                  </Col>
+
+                  <Col span={12}>  
+
+                        <h3>Area</h3>
+                        <Form.Item
+                          name="area"
+                          rules={[
+                            {
+                              type: 'string',
+                              message: 'The input is not valid',
+                            },
+                            {
+                              required: true,
+                              message: 'Please input Area!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder="Area"/>
+                        </Form.Item>
+                        
+                        <h3>Terrace Area</h3>
+                        <Form.Item
+                          name="terraceArea"
+                          rules={[
+                            {
+                              type: 'string',
+                              message: 'The input is not valid',
+                            },
+                            {
+                              required: true,
+                              message: 'Please input Terrace Area!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder="Terrace Area"/>
+                        </Form.Item>
+
+                        <h3>Balcony Area</h3>
+                        <Form.Item
+                          name="balconyArea"
+                          rules={[
+                            {
+                              type: 'string',
+                              message: 'The input is not valid',
+                            },
+                            {
+                              required: true,
+                              message: 'Please input Balcony Area!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder="Balcony Area"/>
+                        </Form.Item>
+                        
+                        <h3>Description</h3>
+                      <Form.Item name= "description">
+                        <TextArea rows={4} />
+                      </Form.Item>
+
+
+                    
+                  </Col>
+                </Row>
+                  <div >
                   <Form.Item>
                     <Button type="primary" htmlType="submit">
                       Confirm
                     </Button>
                   </Form.Item>
-                </Form>
-              </Col>
-            </Row>
+                  </div>
+            </Form>
           </div>
         </Card>
       </div>
