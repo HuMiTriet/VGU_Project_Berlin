625426b30284a0b7647bef8730dbd22c5a87d6c0
Triet Huynh <triet403@gmail.com>
2022-10-31 14:43:28 +0700

    remove login path

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Contract from './pages/Contract/Contract'
 import Dashboard from './pages/Dashboard/Dashboard'
-import Login from './Authentication/Login/Login'
 import PropertyDetails from './pages/PropertyDetail/PropertyDetail'
 import InfoUpload from './pages/InfoUpload/InfoUpload'
 
@@ -13,7 +12,6 @@ export function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/propertyinfo" element={<PropertyDetails />} />
           <Route path="/contract" element={<Contract />} />
